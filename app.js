59eccf881cdf1e8e9b807621472daabe4471f9a3
muskanpatel3 <muskanpatel03032003@gmail.com>
2026-04-28 17:17:00 +0530

    updated project

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ async function main() {
 );
 }
 
-app.set("view engine", "ejs");
+app.set("view engine", "ejs");                
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
